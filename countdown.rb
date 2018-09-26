c05e9abc07ba11ddf0565ea2aba666f005c96ce6
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-26 05:34:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,10 @@
 #write your code here
 
-def countdown(int)
-  
-  while
+def countdown(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
